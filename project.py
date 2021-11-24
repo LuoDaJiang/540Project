2a005dccebbc6b80ff68fd13258583a6644353dc
yayuntia <yayuntian495@gmail.com>
2021-11-24 00:09:28 -0800

    add tfidf

--- project.py
+++ project.py
@@ -4,34 +4,30 @@ from gensim import corpora
 import re
 import nltk
 from nltk.stem import WordNetLemmatizer
+from gensim import models
 from spellchecker import SpellChecker
+import csv
+from gensim.models import LdaModel, LdaMulticore
 
-TotalPath="/Users/qwertyui/我爱学习/540/project/DataFinal/complaints_Cleaned_without200.csv"
-savePath="/Users/qwertyui/我爱学习/540/project/DataFinal/narratives.csv"
+TotalPath="/Users/qwertyui/我爱学习/540/project/DataFinal/Cleaned_random200 (1).csv"
+savePath="/Users/qwertyui/我爱学习/540/project/DataFinal/tf-idf_random200.csv"
+narrativepath="/Users/qwertyui/我爱学习/540/project/DataFinal/narratives.csv"
 
 Total=pd.read_csv(TotalPath)
 Total.columns = [c.replace(' ', '_') for c in Total.columns]
 na=Total['Consumer_complaint_narrative']
-# na.to_csv(savePath,index=False)
+#na.to_csv(savePath,index=False)
 documents=na.values.tolist()
+documents=documents[:10]
 # documents=narrative[0:10]
 
-#cleaning based on string
-# for doc in documents:
-#     doc = re.sub('[^A-Za-z0-9]+', ' ', doc)     ###Remove Punctuations
-#     doc = re.sub("X.*X", "", doc)               ### Remove XXXX
-#     doc=doc.lower()                             ###Convert to lower case
-#     doc=re.sub("\s+"," ",doc)                   ###Removing extra spaces
-#                                                 ###Standardizing and Spell Check
-documents = [re.sub('[^A-Za-z0-9]+', ' ', doc) for doc in documents]
-####documents=[re.sub("X.*X", "", doc) for doc in documents]
-documents=[doc.lower() for doc in documents]
-documents=[re.sub("\s+"," ",doc) for doc in documents]
+###cleaning based on string
+documents = [re.sub('[^A-Za-z0-9]+', ' ', doc) for doc in documents]    ###Remove Punctuations
+#ocuments=[re.sub("X.*X", "", doc) for doc in documents]
+documents=[doc.lower() for doc in documents]                            ###Convert to lower case
+documents=[re.sub("\s+"," ",doc) for doc in documents]                  ###Removing extra spaces
 
-
-
-# Tokenize(split) the sentences into words
-
+### Tokenize(split) the sentences into words
 texts = [[text for text in doc.split()] for doc in documents]
 
 #cleaning based on words
@@ -48,7 +44,17 @@ dictionary = corpora.Dictionary(texts)
 mycorpus = [dictionary.doc2bow(doc, allow_update=True) for doc in texts]
 #print(dictionary)
 #print(dictionary.token2id)
-from gensim.models import LdaModel, LdaMulticore
+
+tfidf = models.TfidfModel(mycorpus)
+vectors = [tfidf[word] for word in mycorpus]
+with open(savePath, 'w') as f:
+    # using csv.writer method from CSV package
+    write = csv.writer(f)
+
+    write.writerow(['tf-idf'])
+    write.writerows(vectors)
+
+
 lda_model = LdaMulticore(corpus=mycorpus,
                          id2word=dictionary,
                          random_state=100,
